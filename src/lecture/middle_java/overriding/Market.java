4e9691ca06273b56ad87bf0bde0794e98181d7f9
flature <jungwoo5394@gmail.com>
2021-03-12 22:49:04 +0900

    생성자 오버로딩 자료 업데이트

--- src/lecture/middle_java/overriding/Market.java
+++ src/lecture/middle_java/overriding/Market.java
@@ -2,8 +2,8 @@ package lecture.middle_java.overriding;
 
 public interface Market {
 
-    public void stack(String product, int quantity, int value);
+    void stack(String product, int quantity, int value);
 
-    public void  buy(String product);
+    void  buy(String product);
 
 }
