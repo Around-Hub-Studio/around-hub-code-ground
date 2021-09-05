bb647c091f30863bd50dafa69a7573f1d6fb4526
Flature <jungwoo5394@gmail.com>
2021-09-05 18:53:41 +0900

    Collection List 인터페이스 관련 코드 업데이트

---
+++ src/main/java/lecture/collection/CollectionExample.java
@@ -0,0 +1,34 @@
+package lecture.collection;
+
+import java.util.*;
+
+public class CollectionExample {
+
+    public static void main(String[] args){
+
+        Stack stack;
+
+        Deque deque;
+
+        ArrayList arrayList;
+
+        LinkedList linkedList;
+
+        Vector vector;
+
+        PriorityQueue priorityQueue;
+
+        SortedSet sortedSet;
+
+        HashSet hashSet;
+
+        TreeMap treeMap;
+
+        HashMap hashMap;
+
+        SortedMap sortedMap;
+
+        Hashtable hashtable;
+
+    }
+}
---
+++ src/main/java/lecture/collection/ListExample.java
@@ -0,0 +1,122 @@
+package lecture.collection;
+
+import java.util.*;
+
+public class ListExample {
+
+    public static void main(String[] args) {
+
+        System.out.println("-------------------------------↓↓↓ ArrayList ↓↓↓--------------------------");
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        arrayList.add(100);
+        arrayList.add(400);
+        arrayList.add(200);
+        arrayList.add(300);
+        arrayList.add(600);
+
+        for (Integer integer : arrayList) {
+            System.out.println(integer);
+        }
+
+        System.out.println("--------------------------");
+
+/*
+        for(int i = 0 ; i < arrayList.size(); i++){
+            System.out.println(arrayList.get(i));
+        }
+*/
+
+        arrayList.remove(3);
+
+        for (Integer integer : arrayList) {
+            System.out.println(integer);
+        }
+
+        System.out.println("--------------------------");
+
+        Collections.sort(arrayList);
+
+        for (Integer integer : arrayList) {
+            System.out.println(integer);
+        }
+
+        arrayList.set(1, 500);
+
+        System.out.println("--------------------------");
+
+        Iterator iterator = arrayList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        System.out.println(arrayList.size());
+
+        System.out.println("-------------------------------↑↑↑ ArrayList ↑↑↑--------------------------");
+        System.out.println("-------------------------------↓↓↓ LinkedList ↓↓↓--------------------------");
+
+        LinkedList<String> linkedList = new LinkedList<>();
+
+        linkedList.add("가");
+        linkedList.add("다");
+        linkedList.add("나");
+        linkedList.add("마");
+        linkedList.add("라");
+
+        for (int i = 0; i < linkedList.size(); i++) {
+            System.out.println(linkedList.get(i));
+        }
+
+        System.out.println("--------------------------");
+
+        linkedList.remove(3);
+
+        for (String s : linkedList) {
+            System.out.println(s);
+        }
+
+        System.out.println("-------------------------------↑↑↑ LinkedList ↑↑↑--------------------------");
+        System.out.println("-------------------------------↓↓↓ Vector ↓↓↓--------------------------");
+
+        Vector<String> vector = new Vector<>();
+
+        System.out.println(vector.size());
+        System.out.println(vector.capacity());
+        vector.add("가");
+        vector.add("다");
+        System.out.println(vector.add("나"));
+        System.out.println(vector.add("마"));
+        System.out.println(vector.add("라"));
+        System.out.println(vector.size());
+
+        for (String s : vector) {
+            System.out.println(s);
+        }
+
+        System.out.println("-------------------------------↑↑↑ Vector ↑↑↑--------------------------");
+        System.out.println("-------------------------------↓↓↓ Stack ↓↓↓--------------------------");
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.add(1);
+        stack.push(3);
+
+        System.out.println(stack.add(7));
+        System.out.println(stack.push(2));
+
+        System.out.println(stack.size());
+
+        System.out.println(stack.peek());
+
+        System.out.println(stack.size());
+
+        System.out.println(stack.pop());
+
+        System.out.println(stack.size());
+
+        System.out.println("-------------------------------↑↑↑ Stack ↑↑↑--------------------------");
+
+    }
+
+}
