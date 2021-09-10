5598306f6b27cb63316f937a9ffd316a97b4f589
Flature <jungwoo5394@gmail.com>
2021-09-10 20:59:10 +0900

    백준 문제 풀이 코드 업데이트

---
+++ src/main/java/lecture/baekjoon/Main10814.java
@@ -0,0 +1,44 @@
+package lecture.baekjoon;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main10814 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        Man[] men = new Man[N];
+        for (int i = 0; i < N; i++) {
+            men[i] = new Man(sc.nextInt(), sc.next());
+        }
+
+        Arrays.sort(men, new Comparator<Man>() {
+            @Override
+            public int compare(Man o1, Man o2) {
+                return o1.age - o2.age;
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(men[i].age + " " + men[i].name);
+        }
+    }
+
+    public static class Man {
+
+        int age;
+        String name;
+
+        public Man(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+    }
+
+}
---
+++ src/main/java/lecture/baekjoon/Main11650.java
@@ -0,0 +1,35 @@
+package lecture.baekjoon;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main11650 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(arr, (e1, e2) -> {
+            if (e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
+            }
+        });
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+
+}
