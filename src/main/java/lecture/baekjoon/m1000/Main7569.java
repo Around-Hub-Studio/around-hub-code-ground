8089d8890c63a997d37ad51d570e12ff710f8b57
Flature <jungwoo5394@gmail.com>
2021-10-02 03:08:00 +0900

    백준 문제풀이 코드 업데이트

---
+++ src/main/java/lecture/baekjoon/m1000/Main7569.java
@@ -0,0 +1,115 @@
+package lecture.baekjoon.m1000;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main7569 {
+
+    static int M, N, H;
+    static Queue<Integer> xQueue = new LinkedList<>();
+    static Queue<Integer> yQueue = new LinkedList<>();
+    static Queue<Integer> zQueue = new LinkedList<>();
+
+    static int[][][] box;
+
+    static int[] dx = {0, 0, 0, 0, 1, -1};
+    static int[] dy = {0, 0, 1, -1, 0, 0};
+    static int[] dz = {1, -1, 0, 0, 0, 0};
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        M = sc.nextInt();
+        N = sc.nextInt();
+        H = sc.nextInt();
+
+        box = new int[H][N][M];
+
+        for (int i = 0; i < H; i++) { // 층
+            for (int j = 0; j < N; j++) { // 세로
+                for (int k = 0; k < M; k++) { // 가로
+                    box[i][j][k] = sc.nextInt();
+
+                    if (box[i][j][k] == 1) {
+                        xQueue.add(k);
+                        yQueue.add(j);
+                        zQueue.add(i);
+                    }
+                }
+            }
+        }
+
+        bfs();
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < H; i++) { // 층
+            for (int j = 0; j < N; j++) { // 세로
+                for (int k = 0; k < M; k++) { // 가로
+                    int value = box[i][j][k];
+                    if (value == 0) {
+                        System.out.println(-1);
+                        return;
+                    }
+                    max = Math.max(value, max);
+                }
+            }
+        }
+
+        System.out.println(max - 1);
+
+    }
+
+    static void bfs() {
+
+        while (!xQueue.isEmpty() && !yQueue.isEmpty() && !zQueue.isEmpty()) {
+            //System.out.println("Check1");
+
+            int x = xQueue.poll();
+            int y = yQueue.poll();
+            int z = zQueue.poll();
+
+            //System.out.println("x : " + x + ", y : " + y + ", z : " + z);
+            //System.out.println("H : " + H + ", M : " + M + ", N : " + N);
+
+           /* for (int i = 0; i < H; i++) { // 층
+                System.out.println("# " + (i + 1) + "층 #");
+                for (int j = 0; j < N; j++) { // 세로
+                    for (int k = 0; k < M; k++) { // 가로
+                        System.out.print(box[i][j][k] + " ");
+                    }
+                    System.out.println();
+                }
+                System.out.println();
+            }*/
+
+            //System.out.println("Check2");
+
+            for (int i = 0; i < dx.length; i++) {
+                //System.out.println("Check3");
+
+                int xTemp = x + dx[i];
+                int yTemp = y + dy[i];
+                int zTemp = z + dz[i];
+
+                if (xTemp >= 0 && yTemp >= 0 && zTemp >= 0 && xTemp < M && yTemp < N && zTemp < H) {
+                    // 보고 있는 위치에 익지 않은 토마토가 있다면
+                    if (box[zTemp][yTemp][xTemp] == 0) {
+
+                        xQueue.add(xTemp);
+                        yQueue.add(yTemp);
+                        zQueue.add(zTemp);
+
+                        box[zTemp][yTemp][xTemp] = box[z][y][x] + 1;
+
+                    }
+                }
+
+            }
+
+        }
+
+    }
+
+}
---
+++ src/main/java/lecture/baekjoon/m10000/Main11725.java
@@ -0,0 +1,62 @@
+package lecture.baekjoon.m10000;
+
+import java.util.*;
+
+public class Main11725 {
+
+    static int N;
+
+    static ArrayList<Integer>[] arr;
+    static boolean[] visited;
+    static int[] ans;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+
+        arr = new ArrayList[N + 1];
+        visited = new boolean[N+1];
+        ans = new int[N+1];
+
+        for (int i = 0; i < N + 1; i++) {
+            arr[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+
+            arr[start].add(end);
+            arr[end].add(start);
+        }
+
+        bfs();
+
+        for(int i = 2; i<ans.length;i++){
+            System.out.println(ans[i]);
+        }
+    }
+
+    static void bfs(){
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(1);
+        visited[1] = true;
+
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+
+            for(int i = 0; i < arr[node].size(); i++){
+                if(!visited[arr[node].get(i)]){
+                    queue.add(arr[node].get(i));
+                    visited[arr[node].get(i)] = true;
+                    ans[arr[node].get(i)] = node;
+                }
+            }
+        }
+
+    }
+
+}
