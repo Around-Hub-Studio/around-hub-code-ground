ebae4779fcfccff516450d89798997d0596c1749
Flature <jungwoo5394@gmail.com>
2021-10-07 19:52:06 +0900

    테스트 코드 관련 예제 업데이트

---
+++ src/main/java/lecture/test_example/Calculator.java
@@ -0,0 +1,13 @@
+package lecture.test_example;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int multiple(int a, int b) {
+        return a * b;
+    }
+
+}
---
+++ src/test/java/lecture/test_example/CalculatorTest.java
@@ -0,0 +1,27 @@
+package lecture.test_example;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void addTest(){
+        int actual =calculator.add(1, 2);
+        int expected = 3;
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void multipleTest(){
+        int actual =calculator.multiple(1, 2);
+        int expected = 2;
+
+        assertThat(actual, is(expected));
+    }
+
+}
