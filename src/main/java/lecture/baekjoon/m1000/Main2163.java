eb9d6ddb4643c08d0f10372d6f3446d60d6068c3
Flature <jungwoo5394@gmail.com>
2021-10-15 10:42:53 +0900

    백준 문제 풀이 코드 업데이트

---
+++ src/main/java/lecture/baekjoon/m1000/Main2163.java
@@ -0,0 +1,18 @@
+package lecture.baekjoon.m1000;
+
+import java.util.Scanner;
+
+public class Main2163 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        System.out.println(N * M - 1);
+
+    }
+
+}
