a5ab8eefc2cc5b8ed2a98884839eb3e5716c9241
flature <jungwoo5394@gmail.com>
2022-11-19 17:48:04 +0900

    [update] bigdecimal example

---
+++ src/main/java/studio/aroundhub/codeground/decimal/BigDecimalExample.java
@@ -0,0 +1,18 @@
+package studio.aroundhub.codeground.decimal;
+
+import java.math.BigDecimal;
+
+public class BigDecimalExample {
+
+    public static void main(String[] args){
+        BigDecimal plusBigDecimal = new BigDecimal("0");
+        for(int i = 0; i< 100; i++){
+            plusBigDecimal = plusBigDecimal.add(new BigDecimal("0.1"));
+        }
+        BigDecimal multipleBigDecimal = new BigDecimal("10.0");
+
+        System.out.println("plusBigDecimal : " + plusBigDecimal);
+        System.out.println("multipleBigDecimal : " + multipleBigDecimal);
+    }
+
+}
